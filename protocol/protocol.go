0312db8ee2e3594aca7ae7d5647563e0f9653057
Luc Heijst <ljm.heijst@gmail.com>
2020-05-11 10:52:53 -0300

    Update protocol.go

--- protocol/protocol.go
+++ protocol/protocol.go
@@ -19,7 +19,7 @@
    Added: EU-frequencies
    Removed: frequency correction
    Removed: parsing
-   VERSION: 0.10
+   VERSION: 0.12
 */
 package protocol
 
@@ -53,6 +53,8 @@ type Parser struct {
 	hopPattern		[]int
 	reverseHopPatrn []int
 	freqError		int
+	channel			int
+	transmitter		int
 	chfreq			int
 	freqerrTrChList [8][51][10]int
 	freqerrTrChPtr	[8][51]int
@@ -79,24 +81,42 @@ func NewParser(symbolLength int, tf string) (p Parser) {
 			0, 3, 1, 4, 2,   
 		}
 	} else {
-		p.channels = []int{
-			// Thanks to Paul Anderson and Rich T for testing the US frequencies
-			902419338, 902921088, 903422839, 903924589, 904426340, 904928090, // US freq per 20190326
-			905429841, 905931591, 906433342, 906935092, 907436843, 907938593, 
-			908440344, 908942094, 909443845, 909945595, 910447346, 910949096, 
-			911450847, 911952597, 912454348, 912956099, 913457849, 913959599, 
-			914461350, 914963100, 915464850, 915966601, 916468351, 916970102, 
-			917471852, 917973603, 918475353, 918977104, 919478854, 919980605, 
-			920482355, 920984106, 921485856, 921987607, 922489357, 922991108, 
-			923492858, 923994609, 924496359, 924998110, 925499860, 926001611, 
-			926503361, 927005112, 927506862,  
+		if tf == "NZ" {
+			p.channels = []int{
+				// Thanks to Ian Rees for testing the NZ frequencies
+				902419338, 902921088, 903422839, 903924589, 904426340, 904928090, // NZ freq (to be detected)
+				905429841, 905931591, 906433342, 906935092, 907436843, 907938593, 
+				908440344, 908942094, 909443845, 909945595, 910447346, 910949096, 
+				911450847, 911952597, 912454348, 912956099, 913457849, 913959599, 
+				914461350, 914963100, 915464850, 915966601, 916468351, 916970102, 
+				917471852, 917973603, 918475353, 918977104, 919478854, 919980605, 
+				920482355, 920984106, 921485856, 921987607, 922489357, 922991108, 
+				923492858, 923994609, 924496359, 924998110, 925499860, 926001611, 
+				926503361, 927005112, 927506862,  
+			}
+			p.ChannelCount = len(p.channels)
+			p.hopIdx = rand.Intn(p.ChannelCount)
+		} else {
+			p.channels = []int{
+				// Thanks to Paul Anderson and Rich T for testing the US frequencies
+				902419338, 902921088, 903422839, 903924589, 904426340, 904928090, // US freq per 20190326
+				905429841, 905931591, 906433342, 906935092, 907436843, 907938593, 
+				908440344, 908942094, 909443845, 909945595, 910447346, 910949096, 
+				911450847, 911952597, 912454348, 912956099, 913457849, 913959599, 
+				914461350, 914963100, 915464850, 915966601, 916468351, 916970102, 
+				917471852, 917973603, 918475353, 918977104, 919478854, 919980605, 
+				920482355, 920984106, 921485856, 921987607, 922489357, 922991108, 
+				923492858, 923994609, 924496359, 924998110, 925499860, 926001611, 
+				926503361, 927005112, 927506862,  
+			}
+			p.ChannelCount = len(p.channels)
+			p.hopIdx = rand.Intn(p.ChannelCount)
 		}
-		p.ChannelCount = len(p.channels)
-		p.hopIdx = rand.Intn(p.ChannelCount)
+		// Both NZ and US use the same hop sequence
 		p.hopPattern = []int{
-			0, 19, 41, 25, 8, 47, 32, 13, 36, 22, 3, 29, 44, 16, 5, 27, 38, 10,
-			49, 21, 2, 30, 42, 14, 48, 7, 24, 34, 45, 1, 17, 39, 26, 9, 31, 50,
-			37, 12, 20, 33, 4, 43, 28, 15, 35, 6, 40, 11, 23, 46, 18,
+			0, 19, 41, 25, 8, 47, 32, 13, 36, 22, 3, 29, 44, 16, 5, 27, 38,
+			10, 49, 21, 2, 30, 42, 14, 48, 7, 24, 34, 45, 1, 17, 39, 26, 9,
+			31, 50, 37, 12, 20, 33, 4, 43, 28, 15, 35, 6, 40, 11, 23, 46, 18,
 		}
 		p.reverseHopPatrn = []int{
 			0, 29, 20, 10, 40, 14, 45, 25, 4, 33, 17, 47, 37, 7, 23, 43, 13, 
@@ -111,11 +131,12 @@ type Hop struct {
 	ChannelIdx  int
 	ChannelFreq int
 	FreqError   int
+	Transmitter int
 }
 
 func (h Hop) String() string {
-	return fmt.Sprintf("{ChannelIdx:%d ChannelFreq:%d FreqError:%d}",
-		h.ChannelIdx, h.ChannelFreq, h.FreqError,
+	return fmt.Sprintf("{ChannelIdx:%d ChannelFreq:%d FreqError:%d Transmitter:%d}",
+		h.ChannelIdx, h.ChannelFreq, h.FreqError, h.Transmitter,
 	)
 }
 
@@ -123,12 +144,16 @@ func (p *Parser) hop() (h Hop) {
 	h.ChannelIdx = p.hopPattern[p.hopIdx]
 	h.ChannelFreq = p.channels[h.ChannelIdx]
 	h.FreqError = p.freqError
+	h.Transmitter = p.transmitter
 	return h
 }
 
 // Set the pattern index and return the new channel's parameters.
-func (p *Parser) SetHop(n int) Hop {
+func (p *Parser) SetHop(n int, tr int) Hop {
 	p.hopIdx = n % p.ChannelCount
+	p.channel = p.hopPattern[p.hopIdx]
+	p.freqError = p.freqerrTrChSum[tr][p.channel] / 10
+	p.transmitter = tr
 	return p.hop()
 }
 
@@ -187,7 +212,6 @@ func (p *Parser) Parse(pkts []dsp.Packet) (msgs []Message) {
 		p.freqerrTrChSum[tr][ch] = p.freqerrTrChSum[tr][ch] - p.freqerrTrChList[tr][ch][p.freqerrTrChPtr[tr][ch]] + freqerr
 		p.freqerrTrChList[tr][ch][p.freqerrTrChPtr[tr][ch]] = freqerr
 		p.freqerrTrChPtr[tr][ch] = (p.freqerrTrChPtr[tr][ch] + 1) % p.maxTrChList
-		p.freqError = p.freqerrTrChSum[tr][ch] / 10
 //		log.Printf("tr=%d ch=%d old=%d freqerr=%d avgfreqErr=%d ptr=%d sum=%d", tr, ch, old, freqerr, p.freqError, p.freqerrTrChPtr[tr][ch], p.freqerrTrChSum[tr][ch])
 //		log.Printf("freqerrTrChList=%d", p.freqerrTrChList[tr][ch])
 	}
