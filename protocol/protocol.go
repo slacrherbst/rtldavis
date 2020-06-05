b95d5d734e4666c90f3d7539d5e2acd9f80f7e43
Luc Heijst <ljm.heijst@gmail.com>
2020-06-05 08:43:42 -0300

    Version 0.15

--- protocol/protocol.go
+++ protocol/protocol.go
@@ -1,37 +1,45 @@
 /*
-   rtldavis, an rtl-sdr receiver for Davis Instruments weather stations.
-   Copyright (C) 2015  Douglas Hall
+	rtldavis, an rtl-sdr receiver for Davis Instruments weather stations.
+	Copyright (C) 2015 Douglas Hall
 
-   This program is free software: you can redistribute it and/or modify
-   it under the terms of the GNU General Public License as published by
-   the Free Software Foundation, either version 3 of the License, or
-   (at your option) any later version.
+	This program is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
 
-   This program is distributed in the hope that it will be useful,
-   but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-   GNU General Public License for more details.
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+	GNU General Public License for more details.
 
-   You should have received a copy of the GNU General Public License
-   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+	You should have received a copy of the GNU General Public License
+	along with this program. If not, see <http://www.gnu.org/licenses/>.
 
-   Modified by Luc Heijst - March 2019
-   Added: EU-frequencies
-   Removed: frequency correction
-   Removed: parsing
-   VERSION: 0.12
+	Modified by Luc Heijst - March 2019
+	Version: 0.13 - March 2019
+		Added: EU-frequencies
+		Removed: frequency correction
+		Removed: parsing
+	Version: 0.14 - May 2020
+		Added: NZ-frequency handling
+		Changed: storage of freqError for the right channer and transmitter 
+	Version: 0.15 - May 2020
+		Changed: an improved calculation of freqError (thanks to Steve Wormley)
+	
 */
 package protocol
 
 import (
 	"fmt"
-//	"log"
+	"log"
 	"math"
 	"math/rand"
 
 	"github.com/lheijst/rtldavis/crc"
 	"github.com/lheijst/rtldavis/dsp"
 )
+
+var Verbose bool
 
 func NewPacketConfig(symbolLength int) (cfg dsp.PacketConfig) {
 	return dsp.NewPacketConfig(
@@ -43,6 +51,10 @@ func NewPacketConfig(symbolLength int) (cfg dsp.PacketConfig) {
 	)
 }
 
+const maxTrCh = 10
+const maxTr   = 8
+const maxCh   = 51
+
 type Parser struct {
 	dsp.Demodulator
 	crc.CRC
@@ -52,21 +64,21 @@ type Parser struct {
 	hopIdx			int
 	hopPattern		[]int
 	reverseHopPatrn []int
-	freqError		int
-	channel			int
+	freqCorr		int
 	transmitter		int
 	chfreq			int
-	freqerrTrChList [8][51][10]int
-	freqerrTrChPtr	[8][51]int
-	freqerrTrChSum	[8][51]int
+	freqerrTrChList [maxTr][maxCh][maxTrCh]int
+	freqerrTrChSum	[maxTr][maxCh]int
+	freqerrTrChPtr	[maxTr][maxCh]int
 	maxTrChList		int
+	factor			float32
 }
 
 func NewParser(symbolLength int, tf string) (p Parser) {
 	p.Cfg = NewPacketConfig(symbolLength)
 	p.Demodulator = dsp.NewDemodulator(&p.Cfg)
 	p.CRC = crc.NewCRC("CCITT-16", 0, 0x1021, 0)
-	p.maxTrChList = 10
+	p.maxTrChList = maxTrCh
 
 	if tf == "EU" {
 		p.channels = []int{ 
@@ -126,31 +138,51 @@ func NewParser(symbolLength int, tf string) (p Parser) {
 }
 
 type Hop struct {
-	ChannelIdx  int
+	ChannelIdx	int
 	ChannelFreq int
-	FreqError   int
+	FreqCorr	int
 	Transmitter int
 }
 
 func (h Hop) String() string {
+    // Note program rtldavis.py parse on text 'FreqError' and not on text 'FreqCorr'
 	return fmt.Sprintf("{ChannelIdx:%d ChannelFreq:%d FreqError:%d Transmitter:%d}",
-		h.ChannelIdx, h.ChannelFreq, h.FreqError, h.Transmitter,
+		h.ChannelIdx, h.ChannelFreq, h.FreqCorr, h.Transmitter,
 	)
 }
 
 func (p *Parser) hop() (h Hop) {
 	h.ChannelIdx = p.hopPattern[p.hopIdx]
 	h.ChannelFreq = p.channels[h.ChannelIdx]
-	h.FreqError = p.freqError
+	h.FreqCorr = p.freqCorr
 	h.Transmitter = p.transmitter
 	return h
 }
 
 // Set the pattern index and return the new channel's parameters.
 func (p *Parser) SetHop(n int, tr int) Hop {
+	// The value of p.factor is experimental; the goal is a steady list of frecErrors over time.
+	p.factor = (float32(p.maxTrChList / 2) + float32(0.5)) * float32(2.0)
 	p.hopIdx = n % p.ChannelCount
-	p.channel = p.hopPattern[p.hopIdx]
-	p.freqError = p.freqerrTrChSum[tr][p.channel] / 10
+	ch := p.hopPattern[p.hopIdx]
+	idx := p.freqerrTrChPtr[tr][ch]
+	// The applied frequency of round (n) is based upon the applied frequency correction
+	// of round (n-1) and the measured freqError of round (n-1).
+	// In fact we have to adjust the original frequency with the sum of all freqCorrections
+	// of round 1 to n.
+	// For practical reasons we only use the history of the freqErrors in the freqerrTrChList.
+	// The elder the freqErrors, the less influence they have in the applied freqCorreection.
+
+	p.freqCorr = 0
+	for i := 0; i < p.maxTrChList; i++ {
+		p.freqCorr = p.freqCorr + (p.freqerrTrChList[tr][ch][idx] * (i+1) / p.maxTrChList)
+		idx = (idx + 1) % p.maxTrChList
+	}
+	p.freqCorr = int(float32(p.freqCorr) / p.factor)
+    // set index to last freqError
+	idx = (idx + p.maxTrChList -1) % p.maxTrChList
+	if (Verbose) {log.Printf("tr=%d ch=%d freqCorr=%d lastFreqError=%d, freqerrTrChList=%d", 
+		tr, ch, p.freqCorr, p.freqerrTrChList[tr][ch][idx], p.freqerrTrChList[tr][ch])}
 	p.transmitter = tr
 	return p.hop()
 }
@@ -186,32 +218,39 @@ func (p *Parser) Parse(pkts []dsp.Packet) (msgs []Message) {
 		if p.Checksum(pkt.Data[2:]) != 0 {
 			continue
 		}
-		// Look at the packet's tail to determine frequency error between
+		// Thanks to Steve Wormley for an improved calculation of freqError.
+		// Look at the packet's preamble to determine frequency error between
 		// transmitter and receiver.
-		lower := pkt.Idx + 8*p.Cfg.SymbolLength
-		upper := pkt.Idx + 24*p.Cfg.SymbolLength
+		// It should have equal ones and zeros so we should average out to 0
+		// Have to stride this at the same as symbol length
+		lower := pkt.Idx + 0*p.Cfg.SymbolLength
+		upper := pkt.Idx + 16*p.Cfg.SymbolLength
 		tail := p.Demodulator.Discriminated[lower:upper]
+		stride := lower % p.Cfg.SymbolLength
+		count := 0
 		var mean float64
-		for _, sample := range tail {
-			mean += sample
+		var discrim [16]float64
+		for i, sample := range tail {
+			if i % p.Cfg.SymbolLength == stride {
+				mean += sample
+				discrim[count]=sample
+				count++
+			}
 		}
-		mean /= float64(len(tail))
+		mean /= float64(count)
+		if (Verbose) {log.Printf("m1: %f l: %d c: %d x: %.2f",mean,len(tail),count,discrim)}
 
-		// The tail is a series of zero symbols. The driminator's output is
+		// The preamble is a set of 0 and 1 symbols, equal in number. The driminator's output is
 		// measured in radians.
-		freqerr := -int(9600 + (mean*float64(p.Cfg.SampleRate))/(2*math.Pi))
+		freqerr := -int((mean*float64(p.Cfg.SampleRate))/(2*math.Pi))
 		msg := NewMessage(pkt)
 		msgs = append(msgs, msg)
 		// Per transmitter and per channel we have a list of p.maxTrChList frequency errors
 		// The average value of the frequencu erreors in the list is used for the frequency correction.
 		tr := int(msg.ID)
 		ch := p.hopPattern[p.hopIdx]
-//		old := p.freqerrTrChList[tr][ch][p.freqerrTrChPtr[tr][ch]]
-		p.freqerrTrChSum[tr][ch] = p.freqerrTrChSum[tr][ch] - p.freqerrTrChList[tr][ch][p.freqerrTrChPtr[tr][ch]] + freqerr
 		p.freqerrTrChList[tr][ch][p.freqerrTrChPtr[tr][ch]] = freqerr
 		p.freqerrTrChPtr[tr][ch] = (p.freqerrTrChPtr[tr][ch] + 1) % p.maxTrChList
-//		log.Printf("tr=%d ch=%d old=%d freqerr=%d avgfreqErr=%d ptr=%d sum=%d", tr, ch, old, freqerr, p.freqError, p.freqerrTrChPtr[tr][ch], p.freqerrTrChSum[tr][ch])
-//		log.Printf("freqerrTrChList=%d", p.freqerrTrChList[tr][ch])
 	}
 	return
 }
